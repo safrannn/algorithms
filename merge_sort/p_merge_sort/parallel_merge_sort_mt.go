f52c71366b40a1dbe81e30f52ccf34f8c3ad538f
safrannn <chengsu6561@gmail.com>
2020-02-10 07:08:01 +0000

    merge sort

--- merge_sort/p_merge_sort/parallel_merge_sort_mt.go
+++ merge_sort/p_merge_sort/parallel_merge_sort_mt.go
@@ -20,30 +20,17 @@ func PMerge(T []int64, leftIndex1 int64, rightIndex1 int64, leftIndex2 int64, ri
 		mid3 := leftIndex3 + (mid1 - leftIndex1) + (mid2 - leftIndex2)
 		A[mid3] = T[mid1]
 
-		// var wgm sync.WaitGroup
-		// wgm.Add(2)
-		// go func() {
-		// 	PMerge(T,leftIndex1, mid1 - 1, leftIndex2, mid2 - 1, A, leftIndex3)
-		// 	wgm.Done()
-		// }()
-		// go func() {
-		// 	PMerge(T,mid1 + 1, rightIndex1, mid2, rightIndex2, A, mid3 + 1)
-		// 	wgm.Done()
-		// }()
-		// wgm.Wait()
-		c1 := make(chan struct{})
-		c2 := make(chan struct{})
+		var wgm sync.WaitGroup
+		wgm.Add(2)
 		go func() {
 			PMerge(T,leftIndex1, mid1 - 1, leftIndex2, mid2 - 1, A, leftIndex3)
-			c1 <- struct{}{}
+			wgm.Done()
 		}()
 		go func() {
 			PMerge(T,mid1 + 1, rightIndex1, mid2, rightIndex2, A, mid3 + 1)
-			c2 <- struct{}{}
+			wgm.Done()
 		}()
-		<-c1
-		<-c2
-
+		wgm.Wait()
 	}
 }
 
@@ -57,23 +44,18 @@ func PMergeSort(A []int64, leftIndex int64, rightIndex int64, B []int64, s int64
 		mid1 := (leftIndex + rightIndex) / 2
 		mid2 := mid1 - leftIndex + 1
 
-		c1 := make(chan struct{})
-		c2 := make(chan struct{})
-		// var wg sync.WaitGroup
-		// wg.Add(2)
+		var wgm sync.WaitGroup
+		wgm.Add(2)
 		go func() {
 			PMergeSort(A, leftIndex, mid1, T, 1)
-			// wg.Done()
-			c1 <- struct{}{}
+			wgm.Done()
 		}()
 		go func() {
 			PMergeSort(A, mid1 + 1, rightIndex, T, mid2 + 1)
-			// wg.Done()
-			c2 <- struct{}{}
+			wgm.Done()
 		}()
-		<-c1
-		<-c2
-		// wg.Wait()
+		wgm.Wait()
+
 		PMerge(T, 1, mid2, mid2 + 1, n, B, s)
 	}
 }
